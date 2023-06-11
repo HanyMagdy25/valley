de99239e19f079d70917f2d9da780bcfa7a5f00f
HanyMagdy25 <hany.magdy25@gmail.com>
2023-06-11 20:52:37 +0300

    footer commit v1

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,8 +2,8 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
-
-
+import logoImage from "../assets/logo.png";
+import Image from "next/image";
 
 const CustomLink = ({ href, title, className = "" }) => {
   const router = useRouter();
@@ -47,87 +47,77 @@ const CustomMobileLink = ({ href, title, className = "", toggle }) => {
 };
 
 const Navbar = () => {
-   
-    const [isOpen, setIsOpen] = useState(false);
-    const handleClick = () => {
-      setIsOpen(!isOpen);
-    };
-    return (
-      <header
-        className="w-full font-medium flex items-center justify-between
-      dark:text-light relative px-32 py-9 z-10 lg:px-16 md:px-12 sm:px-8"
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <header
+      className="w-full font-medium flex items-center justify-between
+      relative px-8 py-1 lg:py-9  z-10  md:px-12 sm:px-8"
+    >
+      <button
+        className="flex lg:hidden flex-col justify-center items-center"
+        onClick={handleClick}
       >
-        <button
-          className="flex lg:hidden flex-col justify-center items-center"
-          onClick={handleClick}
-        >
-          <span
-            className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
-              isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
-            } `}
-          ></span>
-          <span
-            className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${
-              isOpen ? "opacity-0" : "opacity-100"
-            } `}
-          ></span>
-          <span
-            className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
-              isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
-            } `}
-          ></span>
-        </button>
-  
-        <div className="w-full justify-between items-center hidden lg:flex">
-          <nav>
-            <CustomLink href="/" title="الرئيسية" className="mr-4" />
-            <CustomLink href="/about" title="من نحن" className="mx-4" />
-            <CustomLink href="/projects" title="مشاريعنا" className="mx-4" />
-          </nav>
-  
-       
-          NameLogo2
-       
-  
-          <nav className="flex items-center justify-center flex-wrap">
-    
-              TwitterIcon2525
-          
-      
-  
-           
-          </nav>
-        </div>
-  
-        {isOpen ? (
-          <motion.div
-            initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
-            animate={{ scale: 1, opacity: 1 }}
-            className="min-w-[70vw] flex flex-col z-30 justify-between items-center
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+            isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
+          } `}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${
+            isOpen ? "opacity-0" : "opacity-100"
+          } `}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+            isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
+          } `}
+        ></span>
+      </button>
+
+      <div className="w-full justify-between items-center hidden lg:flex">
+        <nav>
+          <CustomLink href="/" title="الرئيسية" className="mr-4" />
+          <CustomLink href="/about" title="من نحن" className="mx-4" />
+          <CustomLink href="/projects" title="مشاريعنا" className="mx-4" />
+        </nav>
+        NameLogo2
+        <nav className="flex items-center justify-center flex-wrap">
+          TwitterIcon2525
+        </nav>
+      </div>
+
+      {isOpen ? (
+        <motion.div
+          initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
+          animate={{ scale: 1, opacity: 1 }}
+          className="min-w-[70vw] flex flex-col z-30 justify-between items-center
             fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/80
           dark:bg-light/75 rounded-lg backdrop-blur-md py-32"
-          >
-            <nav className="flex items-center flex-col justify-center">
-              <CustomMobileLink
-                href="/"
-                title="الرئيسية"
-                className=""
-                toggle={handleClick}
-              />
-              <CustomMobileLink
-                href="/about"
-                title="من نحن"
-                className=""
-                toggle={handleClick}
-              />
-              <CustomMobileLink
-                href="/projects"
-                title="مشاريعنا"
-                className=""
-                toggle={handleClick}
-              />
-            </nav>
-            <nav className="flex items-center justify-center flex-wrap mt-2">
+        >
+          <nav className="flex items-center flex-col justify-center">
+            <CustomMobileLink
+              href="/"
+              title="الرئيسية"
+              className=""
+              toggle={handleClick}
+            />
+            <CustomMobileLink
+              href="/about"
+              title="من نحن"
+              className=""
+              toggle={handleClick}
+            />
+            <CustomMobileLink
+              href="/projects"
+              title="مشاريعنا"
+              className=""
+              toggle={handleClick}
+            />
+          </nav>
+          {/* <nav className="flex items-center justify-center flex-wrap mt-2">
              
                 TwitterIconnnnn
             
@@ -135,18 +125,18 @@ const Navbar = () => {
               
   
               
-            </nav>
-          </motion.div>
-        ) : null}
-  
-        {/* <Logo /> */}
-        <div className="flex lg:hidden ">
-      
-          Logo1
-        </div>
-      </header>
-    );
-  };
-  
-  export default Navbar;
-  
+            </nav> */}
+        </motion.div>
+      ) : null}
+
+      {/* <Logo /> */}
+      <div className="flex lg:hidden ">
+        <span className="text-2xl font-bold">
+          <Image src={logoImage} alt="logoImage" className="w-32" />
+        </span>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
